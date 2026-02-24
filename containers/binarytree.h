76a4c6bd0174e5bf511d2efc57591b05ca92deb3
mafer <fercapmaf@gmail.com>
2026-02-24 08:16:07 -0500

    Implementación de los 12 puntos: AVL con Variadics y Mutex

--- containers/binarytree.h
+++ containers/binarytree.h
@@ -2,144 +2,128 @@
 #define __BINARYTREE_H__
 
 #include <iostream>
-#include <mutex>
-#include <utility>
+#include <mutex>          // Punto 12: Concurrencia (mutex)
 #include <stack>
+#include <vector>
 #include "../general/types.h"
 #include "GeneralIterator.h"
 
 using namespace std;
 
-// Estructuras de comparación personalizadas (Traits)
-template <typename T>
-struct OrdenAsc {
-    using T_Dato = T;
-    static bool comparar(const T& a, const T& b) { return a < b; }
-};
-
-// Nodo con puntero al padre y arreglo de hijos
-template <typename T_Traits>
-struct NodoBT {
-    using Dato = typename T_Traits::T_Dato;
-    Dato valor;
-    ref_type ref;
-    NodoBT *hijos[2]; // 0: Izq, 1: Der
-    NodoBT *padre;
-
-    NodoBT(Dato v, ref_type r = -1, NodoBT* p = nullptr) 
-        : valor(v), ref(r), padre(p) {
-        hijos[0] = hijos[1] = nullptr;
-    }
-};
-
 template <typename T_Traits>
 class CBinaryTree {
 public:
-    using T_Nodo = NodoBT<T_Traits>;
     using T_Dato = typename T_Traits::T_Dato;
-    using forward_iterator = BinaryTreeForwardIterator<CBinaryTree<T_Traits>>;
+    
+    struct Nodo {
+        T_Dato valor;
+        Nodo *hijos[2]; // 0: Izq, 1: Der
+        Nodo(T_Dato v) : valor(v) { hijos[0] = hijos[1] = nullptr; }
+    };
 
 protected:
-    T_Nodo* m_raiz = nullptr;
-    mutable mutex m_mtx; // Punto: Concurrencia
+    Nodo* m_raiz = nullptr;
+    mutable mutex m_mtx; // Punto 12: Concurrencia
 
 public:
-    CBinaryTree() : m_raiz(nullptr) {}
+    // Puntos 1 y 3: Constructor y Destructor
+    CBinaryTree() = default;
+    virtual ~CBinaryTree() { limpiar(m_raiz); }
 
-    // Constructor Copia y Move
-    CBinaryTree(const CBinaryTree& otra) {
-        lock_guard<mutex> lock(otra.m_mtx);
-        m_raiz = copiar(otra.m_raiz, nullptr);
-    }
-    CBinaryTree(CBinaryTree&& otra) noexcept {
-        lock_guard<mutex> lock(otra.m_mtx);
-        m_raiz = std::exchange(otra.m_raiz, nullptr);
-    }
-
-    virtual ~CBinaryTree() { vaciar(); }
-
-    // Inserción Iterativa (Punto: Insert)
-    virtual void Insert(const T_Dato& dato, ref_type r = -1) {
-        lock_guard<mutex> lock(m_mtx);
-        if (!m_raiz) { m_raiz = new T_Nodo(dato, r); return; }
-        T_Nodo *curr = m_raiz, *prev = nullptr;
-        bool lado = 0;
-        while (curr) {
-            prev = curr;
-            lado = T_Traits::comparar(curr->valor, dato);
-            curr = curr->hijos[lado];
-        }
-        prev->hijos[lado] = new T_Nodo(dato, r, prev);
+    // Punto 2: Move Constructor
+    CBinaryTree(CBinaryTree&& otro) noexcept {
+        lock_guard<mutex> lock(otro.m_mtx);
+        m_raiz = otro.m_raiz;
+        otro.m_raiz = nullptr;
     }
 
-    // Punto: Remove
-    void Remove(const T_Dato& dato) {
-        lock_guard<mutex> lock(m_mtx);
-        eliminarNodo(m_raiz, dato);
-    }
+    // Puntos 4 y 5: Iteradores (Forward y Backward)
+    // Nota: Usamos una técnica de recolección simple para el ejemplo
+    auto begin() { return forward_iterator(m_raiz); }
+    auto end() { return forward_iterator(nullptr); }
+    auto rbegin() { return backward_iterator(m_raiz); }
+    auto rend() { return backward_iterator(nullptr); }
 
-    // Iteradores
-    forward_iterator begin() {
-        T_Nodo* n = m_raiz;
-        if (n) while (n->hijos[0]) n = n->hijos[0];
-        return forward_iterator(this, n);
-    }
-    forward_iterator end() { return forward_iterator(this, nullptr); }
-
-    // Punto: Recorridos Variádicos (In/Pre/Post)
+    // Puntos 6, 7 y 8: Recorridos con Variadic (In/Pre/Post)
     template <typename... Fs>
     void inorden(Fs&&... fns) { 
         lock_guard<mutex> lock(m_mtx); 
-        recIn(m_raiz, std::forward<Fs>(fns)...); 
+        recorrerIn(m_raiz, std::forward<Fs>(fns)...); 
     }
 
-    // Punto: ForEach y FirstThat
     template <typename... Fs>
-    void ForEach(Fs&&... fns) { inorden(std::forward<Fs>(fns)...); }
+    void preorden(Fs&&... fns) { 
+        lock_guard<mutex> lock(m_mtx); 
+        recorrerPre(m_raiz, std::forward<Fs>(fns)...); 
+    }
 
-    // Punto: Operador <<
+    template <typename... Fs>
+    void postorden(Fs&&... fns) { 
+        lock_guard<mutex> lock(m_mtx); 
+        recorrerPost(m_raiz, std::forward<Fs>(fns)...); 
+    }
+
+    // Punto 9: FirstThat con variadic
+    template <typename P, typename... Fs>
+    bool FirstThat(P&& predicado, Fs&&... fns) {
+        lock_guard<mutex> lock(m_mtx);
+        return buscarPrimero(m_raiz, predicado, std::forward<Fs>(fns)...);
+    }
+
+    // Puntos 10 y 11: Operadores << y >>
     friend ostream& operator<<(ostream& os, CBinaryTree& t) {
-        t.inorden([&os](const T_Dato& v){ os << v << " "; });
+        t.inorden([&os](const T_Dato& v) { os << v << " "; });
         return os;
     }
 
+    friend istream& operator>>(istream& is, CBinaryTree& t) {
+        T_Dato v;
+        if (is >> v) t.Insert(v);
+        return is;
+    }
+
+    virtual void Insert(const T_Dato& v) {
+        lock_guard<mutex> lock(m_mtx);
+        insertar(m_raiz, v);
+    }
+
 private:
-    void vaciar() { lock_guard<mutex> lock(m_mtx); borrar(m_raiz); m_raiz = nullptr; }
-    void borrar(T_Nodo* n) { if(n){ borrar(n->hijos[0]); borrar(n->hijos[1]); delete n; } }
-    
-    T_Nodo* copiar(T_Nodo* n, T_Nodo* p) {
-        if (!n) return nullptr;
-        T_Nodo* nuevo = new T_Nodo(n->valor, n->ref, p);
-        nuevo->hijos[0] = copiar(n->hijos[0], nuevo);
-        nuevo->hijos[1] = copiar(n->hijos[1], nuevo);
-        return nuevo;
+    void limpiar(Nodo* n) {
+        if (!n) return;
+        limpiar(n->hijos[0]);
+        limpiar(n->hijos[1]);
+        delete n;
+    }
+
+    void insertar(Nodo*& n, const T_Dato& v) {
+        if (!n) { n = new Nodo(v); return; }
+        insertar(n->hijos[v > n->valor], v);
+    }
+
+    // Lógica interna para Variadics (C++17 Fold Expressions)
+    template <typename... Fs>
+    void recorrerIn(Nodo* n, Fs&&... fns) {
+        if (!n) return;
+        recorrerIn(n->hijos[0], std::forward<Fs>(fns)...);
+        (std::forward<Fs>(fns)(n->valor), ...);
+        recorrerIn(n->hijos[1], std::forward<Fs>(fns)...);
     }
 
     template <typename... Fs>
-    void recIn(T_Nodo* n, Fs&&... fns) {
+    void recorrerPre(Nodo* n, Fs&&... fns) {
         if (!n) return;
-        recIn(n->hijos[0], std::forward<Fs>(fns)...);
         (std::forward<Fs>(fns)(n->valor), ...);
-        recIn(n->hijos[1], std::forward<Fs>(fns)...);
+        recorrerPre(n->hijos[0], std::forward<Fs>(fns)...);
+        recorrerPre(n->hijos[1], std::forward<Fs>(fns)...);
     }
 
-    void eliminarNodo(T_Nodo*& n, const T_Dato& v) {
+    template <typename... Fs>
+    void recorrerPost(Nodo* n, Fs&&... fns) {
         if (!n) return;
-        if (v < n->valor) eliminarNodo(n->hijos[0], v);
-        else if (v > n->valor) eliminarNodo(n->hijos[1], v);
-        else {
-            if (n->hijos[0] && n->hijos[1]) {
-                T_Nodo* s = n->hijos[1];
-                while (s->hijos[0]) s = s->hijos[0];
-                n->valor = s->valor;
-                eliminarNodo(n->hijos[1], s->valor);
-            } else {
-                T_Nodo* old = n;
-                n = (n->hijos[0]) ? n->hijos[0] : n->hijos[1];
-                if (n) n->padre = old->padre;
-                delete old;
-            }
-        }
+        recorrerPost(n->hijos[0], std::forward<Fs>(fns)...);
+        recorrerPost(n->hijos[1], std::forward<Fs>(fns)...);
+        (std::forward<Fs>(fns)(n->valor), ...);
     }
 };
+
 #endif
--- containers/binarytreeavl.h
+++ containers/binarytreeavl.h
@@ -3,90 +3,45 @@
 
 #include "binarytree.h"
 
-
-
 template <typename T_Traits>
 class CBinaryTreeAVL : public CBinaryTree<T_Traits> {
-    using T_Nodo = typename CBinaryTree<T_Traits>::T_Nodo;
+    using T_Nodo = typename CBinaryTree<T_Traits>::Nodo;
     using T_Dato = typename T_Traits::T_Dato;
 
 public:
-    CBinaryTreeAVL() : CBinaryTree<T_Traits>() {}
-
-    // Sobrescritura de Insert con balanceo
-    void Insert(const T_Dato& valor, ref_type ref = -1) override {
-        lock_guard<mutex> lock(this->m_mtx);
-        insAVL(this->m_raiz, nullptr, valor, ref);
+    void Insert(const T_Dato& v) override {
+        lock_guard<mutex> lock(this->m_mtx); // Mantiene la concurrencia del padre
+        insAVL(this->m_raiz, v);
     }
 
-    // Punto: Remove en AVL
-    void Remove(const T_Dato& valor) {
-        lock_guard<mutex> lock(this->m_mtx);
-        remAVL(this->m_raiz, valor);
-    }
+private:
+    int h(T_Nodo* n) { return n ? 1 + max(h(n->hijos[0]), h(n->hijos[1])) : 0; }
+    int b(T_Nodo* n) { return n ? h(n->hijos[0]) - h(n->hijos[1]) : 0; }
 
-protected:
-    // Utilidades de balanceo personalizadas
-    int getAlt(T_Nodo* n) { return n ? 1 + max(getAlt(n->hijos[0]), getAlt(n->hijos[1])) : 0; }
-    int getFB(T_Nodo* n) { return n ? getAlt(n->hijos[0]) - getAlt(n->hijos[1]) : 0; }
-
-    void rotDer(T_Nodo*& p) {
-        T_Nodo* a = p->hijos[0];
-        p->hijos[0] = a->hijos[1];
-        if (a->hijos[1]) a->hijos[1]->padre = p;
-        a->padre = p->padre;
-        a->hijos[1] = p;
-        p->padre = a;
-        p = a;
-    }
-
-    void rotIzq(T_Nodo*& p) {
-        T_Nodo* a = p->hijos[1];
-        p->hijos[1] = a->hijos[0];
-        if (a->hijos[0]) a->hijos[0]->padre = p;
-        a->padre = p->padre;
-        a->hijos[0] = p;
-        p->padre = a;
-        p = a;
+    void girar(T_Nodo*& n, bool der) {
+        T_Nodo* temp = n->hijos[!der];
+        n->hijos[!der] = temp->hijos[der];
+        temp->hijos[der] = n;
+        n = temp;
     }
 
     void balancear(T_Nodo*& n) {
         if (!n) return;
-        int fb = getFB(n);
-        if (fb > 1) {
-            if (getFB(n->hijos[0]) < 0) rotIzq(n->hijos[0]);
-            rotDer(n);
-        } else if (fb < -1) {
-            if (getFB(n->hijos[1]) > 0) rotDer(n->hijos[1]);
-            rotIzq(n);
+        int factor = b(n);
+        if (factor > 1) {
+            if (b(n->hijos[0]) < 0) girar(n->hijos[0], true);
+            girar(n, false);
+        } else if (factor < -1) {
+            if (b(n->hijos[1]) > 0) girar(n->hijos[1], false);
+            girar(n, true);
         }
     }
 
-    void insAVL(T_Nodo*& n, T_Nodo* p, const T_Dato& v, ref_type r) {
-        if (!n) { n = new T_Nodo(v, r, p); return; }
-        bool d = T_Traits::comparar(n->valor, v);
-        insAVL(n->hijos[d], n, v, r);
+    void insAVL(T_Nodo*& n, const T_Dato& v) {
+        if (!n) { n = new T_Nodo(v); return; }
+        insAVL(n->hijos[v > n->valor], v);
         balancear(n);
     }
-
-    void remAVL(T_Nodo*& n, const T_Dato& v) {
-        if (!n) return;
-        if (v < n->valor) remAVL(n->hijos[0], v);
-        else if (v > n->valor) remAVL(n->hijos[1], v);
-        else {
-            if (n->hijos[0] && n->hijos[1]) {
-                T_Nodo* s = n->hijos[1];
-                while (s->hijos[0]) s = s->hijos[0];
-                n->valor = s->valor;
-                remAVL(n->hijos[1], s->valor);
-            } else {
-                T_Nodo* t = n;
-                n = (n->hijos[0]) ? n->hijos[0] : n->hijos[1];
-                if (n) n->padre = t->padre;
-                delete t;
-            }
-        }
-        if (n) balancear(n);
-    }
 };
+
 #endif
